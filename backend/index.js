ed1c2ee0aff407edd60f8e945f40e4ca7ef4594a
Nishu Kumari <kumarinishuiiitbh@gmail.com>
2026-08-14 01:52:39 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://talkify-app-qq5q.onrender.com",
     credentials:true
 }))
 app.use(express.json())
